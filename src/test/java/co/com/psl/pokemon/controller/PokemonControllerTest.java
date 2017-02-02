570ac12c7b4605d2a85f115a565e99afbfdd872f
Adrian Jimenez <adrianx30@gmail.com>
2017-02-02 15:42:08 -0500

    Testing methds

--- src/test/java/co/com/psl/pokemon/controller/PokemonControllerTest.java
+++ src/test/java/co/com/psl/pokemon/controller/PokemonControllerTest.java
@@ -31,6 +31,42 @@ public class PokemonControllerTest {
 	    .andExpect(content().json(expected));    
 	}
 	
+	@Test
+	public void getAllShouldReturnAllPokemon() throws Exception{
+		
+		String expected = "[{\"id\":1,\"name\":\"Pikachu\",\"type\":[{\"id\":5,\"type\":\"poison\"},{\"id\":6,\"type\":\"fire\"}],\"weakness\":[{\"id\":6,\"type\":\"fire\"},{\"id\":7,\"type\":\"ice\"},{\"id\":8,\"type\":\"flying\"},{\"id\":9,\"type\":\"physic\"}],\"evolutionId\":3,\"image\":\"http://www.dondevive.org/wp-content/uploads/2016/07/pikachu.jpg\"},{\"id\":2,\"name\":\"Bulbasaur\",\"type\":[{\"id\":5,\"type\":\"poison\"},{\"id\":6,\"type\":\"fire\"}],\"weakness\":[{\"id\":6,\"type\":\"fire\"},{\"id\":7,\"type\":\"ice\"},{\"id\":8,\"type\":\"flying\"},{\"id\":9,\"type\":\"physic\"}],\"evolutionId\":0,\"image\":\"http://www.dondevive.org/wp-content/uploads/2016/07/pikachu.jpg\"}]";	
+		this.mockMvc.perform(get("/pokemon")).andDo(print()).andExpect(status().isOk())
+        .andExpect(content().json(expected));
+	}
+	
+	@Test
+	public void getPokemonByIdShouldReturnPokemon() throws Exception{
+		
+		this.mockMvc.perform(get("/pokemon/1")).andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(1));
+	}
+	
+	@Test
+	public void getTypesShouldReturnAllTypes() throws Exception {
+		
+		String expected = "[{\"id\":1,\"type\":\"normal\"},{\"id\":2,\"type\":\"electric\"},{\"id\":3,\"type\":\"ground\"},{\"id\":4,\"type\":\"grass\"},{\"id\":5,\"type\":\"poison\"},{\"id\":6,\"type\":\"fire\"},{\"id\":7,\"type\":\"ice\"},{\"id\":8,\"type\":\"flying\"},{\"id\":9,\"type\":\"physic\"}]";
+	    this.mockMvc.perform(get("/pokemon/types")).andDo(print()).andExpect(status().isOk())
+	    .andExpect(content().json(expected));    
+	}
+	
+	@Test
+	public void getTypesByIdShouldReturnType() throws Exception{
+		
+		this.mockMvc.perform(get("/pokemon/types/4")).andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(4));
+	}
+	
+	/*
+	@Test
+	public void getAllShouldReturnErrorIfNameNotFound() throws Exception{
+		this.mockMvc.perform(get("/pokemon").param("name", "ThisIsNotAPokemonName")).andDo(print()).andExpect(status().isBadRequest())
+		.andReturn();
+	} */
 	
 
 }
